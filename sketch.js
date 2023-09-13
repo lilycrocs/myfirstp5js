91f62cb452117a095d59b079bd9d9697a450c672
LILY CROCAMO <LC22BG@FSU.EDU>
2023-09-12 20:24:52 -0400

    albers

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
 function setup() {
   createCanvas(400, 400);
+  noStroke()
 }
 
 function draw() {
-  background(125);
+  background(229, 107, 0);
+
+  fill(69, 32, 19, 210)
+  rectMode(CORNER);
+  rect(200, 200, 140, 140,)
+
+  fill(97, 103, 52, 220)
+  rectMode(CORNER);
+  rect(60, 60, 140, 140)
+
+  fill(100, 128, 127, 220)
+  rectMode(CORNER);
+  rect(200, 60, 140, 140)
+
+  fill(233, 219, 179, 220)
+  rectMode(CORNER);
+  rect(60, 200, 140, 140)
+
+  fill(229, 107, 0, 130)
+  rectMode(CENTER);
+  rect(200, 200, 150, 150)
 }
